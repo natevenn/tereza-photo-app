a15ec68e396b2365909c7f5952007c68d88f3562
Nathan Venn <natevenn@gmail.com>
2017-02-07 14:47:35 -0700

    remove link hover on arrow icons

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -11,9 +11,21 @@ export default class Carousel extends Component {
         <div className="carousel-div">
           <img className="carousel-img" src={this.props.imageUrl} role='presentation' />
         </div>
-        <a onClick={this.props.getLastImage} className="left-arrow"> {leftArrow} </a>
-        <a onClick={this.props.getNextImage} className="right-arrow"> {rightArrow} </a>
-        <a onClick={this.props.exitCarousel} className="exitButton"> {exitButton} </a>
+        <a onClick={this.props.getLastImage}>
+          <div className="left-arrow">
+          {leftArrow}
+        </div>
+        </a>
+        <a onClick={this.props.getNextImage}>
+          <div className="right-arrow">
+            {rightArrow}
+          </div>
+      </a>
+      <a onClick={this.props.exitCarousel}>
+        <div className="exitButton">
+          {exitButton}
+        </div>
+        </a>
       </div>
     )
   }
