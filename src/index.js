7e278dc530fe02c77ed4ed8fee62bcf08fba4b88
Nathan Venn <natevenn@gmail.com>
2017-03-18 17:05:08 -0600

    remove firebase keys from index.js

--- src/index.js
+++ src/index.js
@@ -24,15 +24,3 @@ const routes = (
 
 ReactDOM.render( routes, document.getElementById('root') );
       //<Match pattern='/login' component={UploadImage}/>
-//import firebase from 'firebase';
-
-//const config = {
-  //apiKey: "AIzaSyC9higEz5ixX4xa_9_mrTgkxk45t0vJBlM",
-  //authDomain: "tereza-photo-website.firebaseapp.com",
-  //databaseURL: "https://tereza-photo-website.firebaseio.com",
-  //storageBucket: "tereza-photo-website.appspot.com",
-//};
-
-//const fb = firebase.initializeApp(config)
-
-//export default fb;
