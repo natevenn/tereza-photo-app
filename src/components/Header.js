c65fcaa8247da44a456c75440922fbfc1de085c1
Nathan Venn <natevenn@gmail.com>
2017-07-16 20:32:44 -0600

    add link to store

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,13 +26,24 @@ export default class Header extends Component {
     return (
       <div className={className}>
         <div className="logo-div">
-        <a href='/'>
-          <img src={watermark} className="header-logo" />
-        </a>
-      </div>
+          <a href='/'>
+            <img src={watermark} className="header-logo" />
+          </a>
+        </div>
         <div className='navbar'>
           <ul>
             {this.renderList()}
+            <li className='collection'>
+              <button className='online-store-button'>
+                <a
+                  className='online-store-link'
+                  href="https://www.mijnwebwinkel.nl/winkel/t-photography/c-4028478/products/"
+                  target="_blank"
+                >
+                  Online Store
+                </a>
+              </button>
+            </li>
           </ul>
         </div>
       </div>
